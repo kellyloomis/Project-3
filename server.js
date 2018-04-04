39cfa0c051d6d0ea577486b28b81c6d1c8fbc56a
Kist221 <Kist221@gmail.com>
2018-04-03 21:17:38 -0700

    fixed deployment check

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve up static assets
-app.use(express.static("client/build"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Add routes, both API and view
 app.use(routes);
