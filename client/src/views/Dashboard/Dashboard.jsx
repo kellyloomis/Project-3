b7b93e037431150e8062045b8743e0bcde5768eb
JamieDavid <jjdavid14@live.com>
2018-04-10 19:55:08 -0700

    moved table of employees to home page from dashboard

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -125,27 +125,6 @@ class Dashboard extends React.Component {
             />
           </ItemGrid>
         </Grid>
-        <Grid container>
-          <ItemGrid xs={12} sm={12} md={12}>
-            <RegularCard
-              headerColor="orange"
-              cardTitle="Employees Stats"
-              cardSubtitle="New employees on 7th April, 2018"
-              content={
-                <Table
-                  tableHeaderColor="warning"
-                  tableHead={["ID", "Name", "Salary", "Country"]}
-                  tableData={[
-                    ["1", "John Doe", "$36,738", "Niger"],
-                    ["2", "Jane Hope", "$23,789", "Curaçao"],
-                    ["3", "Han Solo", "$56,142", "Netherlands"],
-                    ["4", "Luke Skywalker", "$38,735", "Korea, South"]
-                  ]}
-                />
-              }
-            />
-          </ItemGrid>
-        </Grid>
       </div>
     );
   }
