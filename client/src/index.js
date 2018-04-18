93e24e06fc53a327258d1fd59325ec595c354a10
Kist221 <Kist221@gmail.com>
2018-04-17 21:58:15 -0700

    err trapping

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 
-import Signup from "./views/Signup/Signup.jsx";
-import Landing from "./views/Landing/Landing.jsx";
+import Signup from './views/Signup/Signup.jsx';
+import Landing from './views/Landing/Landing.jsx';
 
-import indexRoutes from "./routes/index.jsx";
+import indexRoutes from './routes/index.jsx';
 
-import "./assets/css/dashboard.css";
+import './assets/css/dashboard.css';
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
-    <Switch>
-    	<Route path="/signup" component={Signup} />
-    	<Route path="/landing" component={Landing} />
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} component={prop.component}  key={key}/>;
-      })}
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+	<Router history={history}>
+		<Switch>
+			<Route path="/signup" component={Signup} />
+			<Route path="/landing" component={Landing} />
+			{indexRoutes.map((prop, key) => {
+				return <Route path={prop.path} component={prop.component} key={key} />;
+			})}
+		</Switch>
+	</Router>,
+	document.getElementById('root')
 );
 registerServiceWorker();
