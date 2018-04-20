e6c9ced40279c3d2579ae6c74b50555e036dcc68
JamieDavid <jjdavid14@live.com>
2018-04-19 19:17:47 -0700

    replaced select dropdown

--- client/src/components/ReviewEntryPage/Category/Category.jsx
+++ client/src/components/ReviewEntryPage/Category/Category.jsx
@@ -4,7 +4,7 @@ import "./Category.css"
 class Category extends Component {
 	render() {
 		return (
-			<div className="w3-third w3-container w3-margin-bottom" key={this.props.key}>
+			<div className="w3-third w3-container w3-margin-bottom">
 				<div className="w3-container w3-white">
 					<button type="button" className="btn btn-info btn-circle btn-xl" id="greenButton"><i className="fa fa-check"></i></button>
 					<button type="button" className="btn btn-info btn-circle btn-xl" id="yellowButton"><i className="fa fa-check"></i></button>
@@ -12,10 +12,6 @@ class Category extends Component {
 					<br/><br/>
 					<p><b>{this.props.title}</b></p>
 					<p>{this.props.text}</p>
-					<div className="form-group">
-			          <label htmlFor="exampleFormControlTextarea1">Comments</label>
-			          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-			        </div>
 				</div>
 			</div>
 		);
