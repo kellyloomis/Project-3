b3b46e2050f19053248f2f63030888f7ae7b7e0f
Kist221 <Kist221@gmail.com>
2018-04-19 20:16:44 -0700

    tracking review subjects and score into state

--- client/src/components/ReviewEntryPage/Category/Category.jsx
+++ client/src/components/ReviewEntryPage/Category/Category.jsx
@@ -1,21 +1,69 @@
-import React, { Component } from "react";
-import "./Category.css"
+import React, { Component } from 'react';
+import './Category.css';
 
 class Category extends Component {
-	render() {
-		return (
-			<div className="w3-third w3-container w3-margin-bottom">
-				<div className="w3-container w3-white">
-					<button type="button" className="btn btn-info btn-circle btn-xl" id="greenButton"><i className="fa fa-check"></i></button>
-					<button type="button" className="btn btn-info btn-circle btn-xl" id="yellowButton"><i className="fa fa-check"></i></button>
-					<button type="button" className="btn btn-info btn-circle btn-xl" id="redButton"><i className="fa fa-check"></i></button>
-					<br/><br/>
-					<p><b>{this.props.title}</b></p>
-					<p>{this.props.text}</p>
-				</div>
-			</div>
-		);
-	} 
+  render() {
+    return (
+      <div className="w3-third w3-container w3-margin-bottom">
+        <div className="w3-container w3-white">
+          <button
+            type="button"
+            className="btn btn-info btn-circle btn-xl"
+            id="greenButton"
+            onClick={score =>
+              this.props.onClick(
+                {
+                  subject: this.props.title,
+                  score: 3
+                },
+                score
+              )
+            }
+          >
+            <i className="fa fa-check" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-info btn-circle btn-xl"
+            id="yellowButton"
+            onClick={score =>
+              this.props.onClick(
+                {
+                  subject: this.props.title,
+                  score: 2
+                },
+                score
+              )
+            }
+          >
+            <i className="fa fa-check" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-info btn-circle btn-xl"
+            id="redButton"
+            onClick={score =>
+              this.props.onClick(
+                {
+                  subject: this.props.title,
+                  score: 1
+                },
+                score
+              )
+            }
+          >
+            <i className="fa fa-check" />
+          </button>
+          <br />
+          <br />
+          <p>
+            <b>{this.props.title}</b>
+          </p>
+          <p>{this.props.text}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Category;
